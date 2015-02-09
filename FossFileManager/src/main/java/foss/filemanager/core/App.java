c4de0f20ef3d653a0837e023b878d36b617ad426
CustomFileManager <customfilemanager@gmail.com>
2015-02-08 22:33:27 -0400

    Project
    init project

---
+++ FossFileManager/src/main/java/foss/filemanager/core/App.java
@@ -0,0 +1,13 @@
+package foss.filemanager.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "FossFileManager" );
+    }
+}
